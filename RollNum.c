43101a5c3efeb6ba6323fe2e12e42a008ecd6bf5
shenluhan <1322894173@qq.com>
2018-12-20 21:01:30 +0800

    add RollNum.c

--- RollNum.c
+++ RollNum.c
@@ -1,5 +1,35 @@
-//å›æ–‡æ•°ï¼šæ­£è¯»ä¸åè¯»ç›¸ç­‰çš„æ•°
-//2018/11/14 æ›´æ–°
+//»ØÎÄÊı£ºÕı¶ÁÓë·´¶ÁÏàµÈµÄÊı
+//2018/11/14 ¸üĞÂ
+#include<stdio.h>
 int main() {
-    //to do sth
+	int n,x[100000];
+	int j=0;
+	int b=0;
+	scanf("%d",&n);
+	int i;
+	for(i=0;1;i++){
+			x[i]=n%10;
+			j++;
+			n=(n-x[i])/10;
+			if(n<10){
+				x[j]=n;
+				if(j%2!=0){
+					int v;
+				for( v=0;v<(j+1)/2;v++){
+					if(x[v]!=x[j-v]) b++;
+				}
+			}
+			else{
+				int p;
+			for(p=0;p<(j/2);p++){
+					if(x[p]!=x[j-p]) b++;
+				}
+			}
+				if(b!=0) printf("no");
+				else printf("yes");	
+				break;
+			}
+		}
+    return 0;
 }
+
