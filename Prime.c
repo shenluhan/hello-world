d23f05ffc708e73ae1c0eaafa4a36d72f9a08bed
shenluhan <1322894173@qq.com>
2018-12-20 21:00:55 +0800

    add Prime.c

--- Prime.c
+++ Prime.c
@@ -1,22 +1,45 @@
-/*æ»¡è¶³ä¸‹åˆ—æ¡ä»¶çš„è‡ªç„¶æ•°ç§°ä¸ºè¶…çº§ç´ æ•°:è¯¥æ•°æœ¬èº«,æ‰€æœ‰æ•°å­—ä¹‹å’Œ,æ‰€æœ‰æ•°å­—ä¹‹ç§¯ä»¥åŠæ‰€æœ‰æ•°å­—çš„å¹³æ–¹å’Œéƒ½æ˜¯ç´ æ•°.ä¾‹å¦‚113å°±æ˜¯ä¸€ä¸ªè¶…çº§ç´ æ•°.æ±‚[100,9999]ä¹‹å†…:(1)è¶…çº§ç´ æ•°çš„ä¸ªæ•°.(2)æ‰€æœ‰è¶…çº§ç´ æ•°ä¹‹å’Œ.(3)æœ€å¤§çš„è¶…çº§ç´ æ•°.*/
-//2018/11/14 æ›´æ–°
-int sum_bit(int num) {
-  return 0;
+/*Âú×ãÏÂÁĞÌõ¼şµÄ×ÔÈ»Êı³ÆÎª³¬¼¶ËØÊı:¸ÃÊı±¾Éí,ËùÓĞÊı×ÖÖ®ºÍ,ËùÓĞÊı×ÖÖ®»ıÒÔ¼°ËùÓĞÊı×ÖµÄÆ½·½ºÍ¶¼ÊÇËØÊı.ÀıÈç113¾ÍÊÇÒ»¸ö³¬¼¶ËØÊı.Çó[100,9999]Ö®ÄÚ:(1)³¬¼¶ËØÊıµÄ¸öÊı.(2)ËùÓĞ³¬¼¶ËØÊıÖ®ºÍ.(3)×î´óµÄ³¬¼¶ËØÊı.*/
+//2018/11/14 ¸üĞÂ
+//int sum_bit(int num) {  return 0;}int multi_bit(int num) {return 0;}int square_sum_bit(int num) {return 0;}bool isprime(int num) {return false;}int main() {if(isprime(113)&&isprime(sum_bit(113))&&isprime(multi_bit(113))&&isprime(square_sum_bit(113)))}
+#include<stdio.h>
+int main(){
+	int n,x[10000],a=0,b=0,c=1,d=0,s=0,t=0,f=0;
+	scanf("%d",&n);
+	int j;
+	for(j=2;j<n;j++){
+		if(n%j==0) a++;
+	}
+		if(a!=0) printf("no\n");
+	else {
+		int i;
+		for(i=0;1;i++){
+			x[i]=n%10;
+			j++;
+			n=(n-x[i])/10;
+			if(n<10){
+				x[j]=n;
+				int q;
+				for(q=0;q<=j;q++){
+					b=b+x[q];
+					c=c*x[q];
+					d=d*d+x[q]*x[q];
+				}
+	int r;
+	for(r=2;r<b;r++){
+		if(b%r==0) s++;
+	}		
+	int o;
+	for(o=2;o<c;o++){
+		if(c%o==0) t++;
+	}		
+	int g;
+	for(g=2;g<d;g++){
+		if(d%g==0) f++;
+	}
+	if(s==0&&t==0&&f==0) printf("yes\n");
+	else printf("no\n");break;
+	}
 }
-
-int multi_bit(int num) {
-  return 0;
 }
-
-int square_sum_bit(int num) {
-  return 0;
-}
-
-bool isprime(int num) {
-  return false;
-}
-
-int main() {
-  if(isprime(113)&&isprime(sum_bit(113))&&isprime(multi_bit(113))&&isprime(square_sum_bit(113)))
-    //to do sth
+	return 0;
 }
